6db042460620b936210e63369e58252de85503c2
itsnotm3h <thewhit3pages@gmail.com>
2024-11-11 12:35:37 +0800

    4.0 - updated front page.

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-export default function HomePage (){
-    return(
+export default function HomePage() {
+    return (
         <>
-        <h1> This is for my homepage</h1>
+            {/* <h1> This is for my homepage</h1> */}
+            <div class="container mainVisual m-auto p-5">
+                <div class="row h-100 w-100">
+                    <div class="col-7 h-100 bg-light">left</div>
+                    <div class="col-5 p-0">
+                        <div class="d-flex flex-wrap h-100 w-100">
+                            <div class="col-12 bg-light py-2 mx-2">1</div>
+                            <div class="col-12 bg-light py-2 mx-2 mt-2">1</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/RegisterPage.jsx
+++ src/RegisterPage.jsx
@@ -4,6 +4,9 @@ export default function RegisterPage(){
     return(
         <>
         <h1>This is the register page</h1>
+        <div>
+            
+        </div>
         </>
     )
 }
