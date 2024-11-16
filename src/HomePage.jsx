0085917ee72ea63d30b70fc29f59308543b81743
itsnotm3h <thewhit3pages@gmail.com>
2024-11-16 13:25:55 +0800

    4.2 - update registration form

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
             {/* <h1> This is for my homepage</h1> */}
             <div className="container mainVisual m-auto p-5">
                 <div className="row h-100 w-100">
-                    <div className="col-7 h-100 bg-light">left</div>
+                    <div className="col-7 h-100 bg-light"></div>
                     <div className="col-5 p-0">
                         <div className="d-flex flex-wrap h-100 w-100">
                             <div className="col-12 bg-light py-2 mx-2">1</div>
--- src/RegisterPage.jsx
+++ src/RegisterPage.jsx
@@ -3,16 +3,27 @@ import { Formik, Field, Form } from 'formik';
 import * as Yup from 'yup';
 
 
+
+
 export default function RegisterPage() {
+
+
+    const minYear = (inputYear)=>{
+
+        const currentYear = new Date().getFullYear();
+        const selectYear = inputYear.getFullYear();
+
+        return currentYear - selectYear;
+
+    };
 
     const initialValues = {
         name: '',
         email: '',
         password: '',
         confirmPassword: '',
-        salutation: '',
-        marketingPreferences: [],
-        country: ''
+        dob:'',
+        marketingPreferences: []
     };
 
     const handleSubmit = (values, formikHelpers) => {
@@ -20,6 +31,13 @@ export default function RegisterPage() {
         console.log('Form values:', values);
         formikHelpers.setSubmitting(false);
     };
+
+    const validationSchema = Yup.object({
+        name: Yup.string().required('How do we address you?'),
+        email:Yup.string().email("Invalid address").required("Email is required."),
+        dob:Yup.date().required("Date is required").min(new Date(), "Date must be in the future").test()
+        // password:Yup.string().min
+    })
 
     return (
         <>
@@ -33,17 +51,15 @@ export default function RegisterPage() {
                     >
                         {(formik) => (
                             <Form className="row">
-                                <div className="d-flex w-100">
-                                    <div className="mb-3 col-2">
-                                    <label for="exampleInputEmail1" class="form-label">Salutations</label>
-                                    <Field  type="text" class="form-control" id="exampleInputEmail1" name="salutation" />
-                                        <div id="nameHelp" class="form-text">How may we adress you.</div>
-                                </div>
-                                <div className="mb-3 ms-3 col-10">
-                                    <label for="exampleInputEmail1" class="form-label">Name</label>
+                                <div className="mb-3 col-10">
+                                <label for="exampleInputEmail1" class="form-label">Name</label>
                                     <Field  type="text" class="form-control" id="exampleInputEmail1" name="name"/>
                                         <div id="nameHelp" class="form-text">A name to leave in history.</div>
-                                </div>
+                                        </div>
+                                <div className="mb-3 col-2">
+                                    <label for="exampleInputEmail1" class="form-label">Birthday</label>
+                                    <Field  type="date" class="form-control" name="dob"/>
+                                        <div id="nameHelp" class="form-text">A name to leave in history.</div>
                                 </div>
                                 <div className="mb-3 col-12">
                                     <label for="exampleFieldEmail1" className="form-label">Email</label>
@@ -64,7 +80,8 @@ export default function RegisterPage() {
                                     <Field type="checkbox" className="form-check-Field" id="exampleCheck1" name="marketingPreferences" value="whatsapp"/>
                                     <label className="form-check-label" for="exampleCheck1">Whatsapp</label>
                                 </div>
-                                <button type="submit" className="btn btn-primary">Submit</button>
+
+                                <button type="submit" className="btn btn-primary" disabled={formik.isSubmitting}>Submit</button>
                             </Form>
                         )}
                     </Formik>
